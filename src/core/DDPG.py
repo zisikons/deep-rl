f18e2ca5997fb3fd4030ee2d68de0ac4be8dc7b7
ziyadsheeba <ziyad.sheeba4@gmail.com>
2021-01-05 18:02:27 +0100

    deleted old replay buffer

--- src/core/DDPG.py
+++ src/core/DDPG.py
@@ -17,43 +17,6 @@ def combined_shape(length, shape=None):
         return (length,)
     return (length, shape) if np.isscalar(shape) else (length, *shape)
 
-class ReplayBufferOld:
-    """
-    Buffer to store trajectories.
-    """
-    def __init__(self, state_dim, act_dim, num_agents, size):
-
-        self.state_buf      = np.zeros(combined_shape(size, state_dim*num_agents), dtype=np.float32)
-        self.act_buf        = np.zeros(combined_shape(size, act_dim*num_agents), dtype=np.float32)
-        self.rew_buf        = np.zeros(size, dtype=np.float32)  #rewards
-        self.next_state_buf = np.zeros(combined_shape(size, state_dim*num_agents), dtype = np.float32)
-        #self.done_buf       = np.zeros(combined_shape(size, num_agents), dtype = np.float32) 
-        self.ptr, self.max_size = 0, size
-
-    def store(self, state, act, rew, next_state):
-        """
-        Append a single timestep to the buffer. This is called at each environment
-        update to store the outcome observed outcome.
-        """
-        # buffer has to have room so you can store
-        assert self.ptr < self.max_size
-
-        self.state_buf[self.ptr,:] = state
-        self.act_buf[self.ptr,:] = act
-        self.rew_buf[self.ptr] = rew
-        self.next_state_buf[self.ptr,:] = next_state
-
-        self.ptr += 1
-
-    def get(self):
-        """
-        Call when the agent is to be updated.
-        """
-        # Buffer has to be full before you can get something from it.
-        data = dict(state= self.state_buf, act=self.act_buf,
-                    rew=self.rew_buf, next_state =self.next_state_buf)
-        return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in data.items()}
-
 class ReplayBuffer:
     """
     Buffer to store trajectories.
@@ -69,7 +32,7 @@ class ReplayBuffer:
     def store(self, state, act, rew, next_state):
         """
         Append a single timestep to the buffer. This is called at each environment
-        update to store the outcome observed outcome.
+        update to store the observed outcome.
         """
         # buffer has to have room so you can store
         if self.ptr == self.max_size:
@@ -87,20 +50,12 @@ class ReplayBuffer:
 
     def get(self):
         """
-        Call after an epoch ends. Resets pointers and returns the buffer contents.
+        Call when updating the agent networks
         """
         data = dict(state= np.concatenate(self.state_buf), act=np.concatenate(self.act_buf),
                     rew=np.concatenate(self.rew_buf), next_state = np.concatenate(self.next_state_buf))
 
         return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in data.items()}
-
-
-
-
-
-
-
-
 
 
 
@@ -238,7 +193,6 @@ class DDPGagent:
         Qvals = self.critic(states, actions).squeeze(1)
         assert Qvals.requires_grad == True
 
-        #with torch.no_grad():
         next_actions = self.actor_target(next_states)
         next_Q = self.critic_target(next_states, next_actions.detach()).squeeze(1)
         Qprime = rewards + self.gamma * next_Q

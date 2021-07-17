3b235a3b4a4783f6fae90803f09de8200b6f7254
anisioti <anisioti@student.ethz.ch>
2021-07-17 14:07:14 +0300

    just commiting so that I don't have conflicts

--- scripts/train_runner.py
+++ scripts/train_runner.py
@@ -3,9 +3,10 @@ import sys
 min_seed = 5
 max_seed = 14
 filename = "train_SafeMADDPG_soft.py" #ziyad 
-#filename = "train_SafeMADDPG_soft_reward.py"
+#filename = "train_SafeMADDPG_soft_reward.py
 #filename = "train_SafeMADDPG_hard.py" #athina
-#filename = "train_MADDPG.py" #kostas
+filename = "train_MADDPG.py"
+#kostas
 
 for i in range(min_seed,max_seed):
     print("running script " + filename  + " with seed " + str(i))

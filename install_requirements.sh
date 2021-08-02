8b35df016591931883cfabf877139080a4e8d9b4
ziyadsheeba <ziyad.sheeba4@gmail.com>
2021-08-02 12:52:22 +0200

    removed unnecessary commenting

--- install_requirements.sh
+++ install_requirements.sh
@@ -10,7 +10,5 @@ pip3 install -e ./multiagent-particle-envs
 pip3 install qpsolvers
 
 # Install required Python Packages 
-# (should be done automatically in multiagent-particle-envs)
-# (but better safe than sorry                              )
 pip3 install -Iv gym==0.10.5
 pip3 install -Iv pyglet==1.3.2
